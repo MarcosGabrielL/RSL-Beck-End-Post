1ef4ee794a7121834eb59b7499808be3d767ece8
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-02-07 12:29:53 -0300

    update postimage

--- src/main/java/com/softsaj/redelivre_post/file/FileController.java
+++ src/main/java/com/softsaj/redelivre_post/file/FileController.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
