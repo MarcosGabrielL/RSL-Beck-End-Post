3afbe0336b6375a7c52436e8f390f73c11f47063
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-02-08 08:27:22 -0300

    Update upload image

--- src/main/java/com/softsaj/redelivre_post/controllers/Post_ImagemController.java
+++ src/main/java/com/softsaj/redelivre_post/controllers/Post_ImagemController.java
@@ -87,7 +87,7 @@ public class Post_ImagemController {
      @PostMapping("/imagem/add/{idpost}")
     @ResponseBody
     public ResponseEntity<ResponseMessage> addImage(
-        @RequestBody("file") MultipartFile file,
+        @RequestParam(value = "file") MultipartFile file,
             @PathVariable("idpost") String idpost) {
         
         FileDB filedb = new FileDB();
