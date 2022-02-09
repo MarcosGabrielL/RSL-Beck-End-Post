986987a695f6a262aff2fb48eb91cf78de43ea51
MarcosGabrielL <marcos_gabrielsaj@hotmail.com>
2022-02-09 08:24:17 -0300

    Update services

--- src/main/java/com/softsaj/redelivre_post/controllers/Post_ImagemController.java
+++ src/main/java/com/softsaj/redelivre_post/controllers/Post_ImagemController.java
@@ -24,7 +24,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("/imagens")
 public class Post_ImagemController {
     
